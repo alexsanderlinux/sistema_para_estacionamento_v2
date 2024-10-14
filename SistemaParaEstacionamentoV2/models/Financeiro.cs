2659f294d19cd6db675f0458f3c702e54b9006cc
alexsanderlinux <alexsanderlinux@gmail.com.br>
2024-10-14 07:12:34 -0300

    first commit

---
+++ SistemaParaEstacionamentoV2/models/Financeiro.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaParaEstacionamentoV2.models
+{
+    public class Financeiro
+    {
+        
+    }
+}
