20e5e9375551597a32b64348761c2e9f26c0b7af
alexsanderlinux <alexsanderlinux@gmail.com.br>
2024-10-15 19:29:14 -0300

    feat: implementação classe financeiro

--- SistemaParaEstacionamentoV2/Program.cs
+++ SistemaParaEstacionamentoV2/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SistemaParaEstacionamentoV2.models;
+
+string opcao;
+bool exibirMenu = true;
+bool menuConfiguracao = true;
+bool menuOperacao = false;
+
+
+Financeiro financeiro = new Financeiro();
+
+//AdministracaoOperacional admOperacional = new AdministracaoOperacional();
+//Atendente novoAtendente = new Atendente();
+//AdministracaoFinanceira admFinanceiro = new AdministracaoFinanceira();
+ 
+
+
+//Declaração de menu
+while(exibirMenu)
+{       
+    if(menuConfiguracao)
+    {
+        Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine("| Menu de configuração inicial do sistema de estacionamento.|");
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine("Digite 0 para realizar a primeira configuração do sistema.");
+    }
+
+    if(menuOperacao)
+    {
+        Console.WriteLine("Menu Operacional:");
+        Console.WriteLine("1 - Cadastrar veiculo:");
+        Console.WriteLine("2 - Listar veiculos no estacionamento:");
+        Console.WriteLine("3 - Retirada de veiculo");
+        Console.WriteLine("4 - Encerrar expediente... ");        
+    }
+
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "0":
+            Console.Clear();
+            //novoAtendente.CadastrarAtendente();
+            //metodo da classe financeiro delegação de atividades financeiras
+            financeiro.ResgatarValores();//entrada de valores financeiros
+            menuConfiguracao = false;
+            Console.Clear();
+            menuOperacao = true;
+            break;
+
+        case "1":
+            Console.Clear();
+            //admOperacional.CadastrarNovoVeiculo();
+            menuConfiguracao = false;
+            break;
+
+        case "2":
+            //admOperacional.ListarVeiculos();
+            menuConfiguracao = false;
+            break;
+
+        case "3":
+            //metodo da classe financeiro delegação de atividades financeiras
+            financeiro.CalcularValorTotal();//saida de valores financeiros
+            //admOperacional.RetiradaDeVeiculo();
+            menuConfiguracao = false;
+            break;
+        
+        case "4":
+            Console.Clear();
+            exibirMenu = false;
+            Console.WriteLine("Encerrar programa");
+            //Environment.Exit(0);
+            break;
+        
+        default:
+            Console.WriteLine("opcao invalida...");
+            break;
+    }
+
+}
--- SistemaParaEstacionamentoV2/models/Financeiro.cs
+++ SistemaParaEstacionamentoV2/models/Financeiro.cs
@@ -7,6 +7,37 @@ namespace SistemaParaEstacionamentoV2.models
 {
     public class Financeiro
     {
-        
-    }
+        public decimal ResgatarValorEntrada = 0;
+        public decimal ResgatarValorPorHora = 0;
+        public decimal ResgatarPermanencia = 0;
+        public decimal TotalCalculado = 0;
+
+        public void ResgatarValores()
+        {
+            Console.WriteLine("Gentileza informar o valor da entrada:");
+            string valEntrada  = Console.ReadLine();
+            this.ResgatarValorEntrada = Convert.ToDecimal(valEntrada);
+            //Console.WriteLine(ResgatarValorEntrada);
+            
+
+
+            
+            Console.WriteLine("Gentileza informar o valor por hora:");
+            string valorPorHora  = Console.ReadLine();
+            this.ResgatarValorPorHora = Convert.ToDecimal(valorPorHora);
+            //Console.WriteLine(ResgatarValorPorHora);
+
+           
+        }
+
+        public void CalcularValorTotal()
+        {
+            Console.WriteLine("Gentileza informar a permanencia do veiculo:");
+            string valPermanencia  = Console.ReadLine();
+            ResgatarPermanencia = Convert.ToDecimal(valPermanencia);
+            this.TotalCalculado = this.ResgatarValorEntrada + (this.ResgatarValorPorHora * this.ResgatarPermanencia); 
+            Console.WriteLine(this.TotalCalculado);           
+        }
+    }   
+
 }
